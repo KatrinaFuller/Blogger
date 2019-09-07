2ead957ee8dca871570a10396a1e7a669580339c
Katrina Fuller <trinafuller@att.net>
2019-09-07 17:15:52 -0600

    made schema in commentService

---
+++ server/services/CommentService.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+const Schema = mongoose.Schema
+const ObjectId = Schema.Types.ObjectId
+
+const _model = new Schema({
+  blogId: { type: ObjectId, ref: 'blog', required: true },
+  body: { type: String, required: true },
+  author: {
+    _id: { type: ObjectId, ref: 'User', required: true },
+    // _id: { type: String, required: true },
+    name: { type: String, required: true }
+  }
+}, { timestamps: true })
+
+export default class CommentService {
+  get repository() {
+    return mongoose.model('comment', _model)
+  }
+}
