e5b7c8435503c6c9570fae24f6bcb21c308a3b4a
Katrina Fuller <trinafuller@att.net>
2019-09-06 19:52:18 -0600

    create, edit, and delete in the BlogController

--- server/controllers/BlogController.js
+++ server/controllers/BlogController.js
@@ -37,7 +37,36 @@ export default class BlogController {
     }
   }
 
+  async create(req, res, next) {
+    try {
+      req.body.authorId = req.session.uid
+      let data = await _blogService.create(req.body)
+      res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
 
+  async edit(req, res, next) {
+    try {
+      let data = await _blogService.findOneAndUpdate({ _id: req.params.id, },
+        req.body, { new: true })
+      if (data) {
+        return res.send(data)
+      }
+      throw new Error("invalid id")
+    } catch (error) {
+      next(error)
+    }
+  }
 
+  async delete(req, res, next) {
+    try {
+      await _blogService.findOneAndRemove({ _id: req.params.id })
+      res.send("Deleted Blog")
+    } catch (error) {
+      next(error)
+    }
+  }
 
 }
