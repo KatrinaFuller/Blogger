c740f5c9636d0298a133f73f41d0c8064d64a34d
Katrina Fuller <trinafuller@att.net>
2019-09-06 19:37:28 -0600

    created BlogService

---
+++ server/services/BlogService.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+const Schema = mongoose.Schema
+const ObjectId = Schema.Types.ObjectId
+
+const _model = new Schema({
+  title: { type: String, maxlength: 60, required: true },
+  summary: { type: String, maxlength: 120, required: true },
+  authorId: { type: ObjectId, ref: 'User', required: true },
+  img: { type: String, required: true },
+  body: { type: String, required: true }
+}, { timestamps: true })
+
+export default class BlogService {
+  get repository() {
+    return mongoose.model('Blog', _model)
+  }
+}
