d286df24162d6d772d97d50f1ca22a9594080a4c
Katrina Fuller <trinafuller@att.net>
2019-09-07 14:19:05 -0600

    created getAll in blogController

---
+++ server/controllers/BlogController.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import ValueService from '../services/ValueService';
+import { Authorize } from '../middleware/authorize.js'
+import BlogService from '../services/BlogService';
+
+let _blogService = new BlogService().repository
+
+export default class BlogController {
+  constructor() {
+    this.router = express.Router()
+      .get('', this.getAll)
+      .get('/:id', this.getById)
+      .use(Authorize.authenticated)
+      .post('', this.create)
+      .put('/:id', this.edit)
+      .delete('/:id', this.delete)
+  }
+
+  async getAll(req, res, next) {
+    try {
+      let data = await _blogService.find({})
+      return res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+
+
+}
